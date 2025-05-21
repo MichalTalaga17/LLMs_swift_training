501ee22f5977d838df7db1f8e7e459c4a0b31bc6
Michał Talaga <michal.talaga.programming@gmail.com>
2025-05-21 22:53:47 +0200

    Initial Commit

---
+++ chater/chaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  chaterApp.swift
+//  chater
+//
+//  Created by Michał Talaga on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct chaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
